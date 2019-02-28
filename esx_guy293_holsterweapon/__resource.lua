2c6534ca4bea0f9664bc0c4426684d3b6895f63d
Guy293 <g.shefer29@gmail.com>
2019-02-28 23:43:33 +0200

    Init3

--- esx_guy293_holsterweapon/__resource.lua
+++ esx_guy293_holsterweapon/__resource.lua
@@ -1,2 +1,15 @@
+----------------------------------------------------------------
+-- Copyright © 2019 by Guy Shefer
+-- Made By: Guy293
+-- GitHub: https://github.com/Guy293
+-- Fivem Forum: https://forum.fivem.net/u/guy293/
+----------------------------------------------------------------
+
+
+
+
+
+
+
 client_script "config.lua"
 client_script "client.lua"
