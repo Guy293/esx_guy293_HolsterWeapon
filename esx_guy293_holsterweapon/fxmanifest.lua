ab9081bc127cafd1a5ba05b68fbf29c409ef8cd8
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-18 08:10:30 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- esx_guy293_holsterweapon/fxmanifest.lua
+++ esx_guy293_holsterweapon/fxmanifest.lua
@@ -4,8 +4,10 @@
 -- GitHub: https://github.com/Guy293
 -- Fivem Forum: https://forum.fivem.net/u/guy293/
 ----------------------------------------------------------------
+fx_version 'bodacious'
+game 'gta5'
 
-client_script {
-	"config.lua",
-	"client.lua"
+client_scripts {
+	'config.lua',
+	'client.lua'
 }
